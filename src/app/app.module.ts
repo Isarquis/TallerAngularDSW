e7e238af17f8e2a1105e7ce2f755c0b4a3a70139
Isabella <i.sarquis@uniandes.edu.co>
2024-04-20 20:25:17 -0500

    Taller Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { SerieModule } from './serie/serie.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SerieModule,
+    HttpClientModule
+
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/serie/serie.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Serie } from './serie';
+import { dataSeries } from './dataSeries';
+import { SerieService } from './serie.service';
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+
+  constructor(private serieServie:SerieService) { }
+  series: Array<Serie> =[];
+  promedio:number=0;
+  
+  getSeries(){
+    this.serieServie.getSeries().subscribe(series => {
+      this.series = series;
+      });
+  }
+  
+  promedioTemporadas(series: Serie[]){
+    let totalTemporadas:number=0;
+    for(let index=0; index<series.length;index++){
+        let serie: Serie= series[index];
+        totalTemporadas+=serie.seasons;
+    }
+    this.promedio= totalTemporadas/series.length;
+}
+
+  ngOnInit() {
+    this.getSeries();
+    this.promedioTemporadas(this.series);
+  }
+
+}
